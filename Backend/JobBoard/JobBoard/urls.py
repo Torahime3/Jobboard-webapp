8e93738a26cce7db2a476954d7c501410a32cbc4
Ariirau <ariirau.fucks@epitech.eu>
2023-10-09 19:52:59 +0200

    Update URL for Django
    Update url to get Http response for localhost/page1 & localhost/page2

--- Backend/JobBoard/JobBoard/urls.py
+++ Backend/JobBoard/JobBoard/urls.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('my_api.urls')),
+    path('my_api/', include('my_api.urls')),
     path('admin/', admin.site.urls),
 ]
+
