0bc78f8f464e9d1f0dbdfb1654da0b6ba18d89b7
Ariirau <ariirau.fucks@epitech.eu>
2023-10-10 13:47:23 +0200

    Add new model JobApplicantions

--- Backend/JobBoard/JobApplications/models.py
+++ Backend/JobBoard/JobApplications/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from JobAdvertisements.models import JobAdvertisements
+from Peoples.models import Peoples
 
 # Create your models here.
+class JobApplications(models.Model):
+    types = models.TextChoices("Types", "CDD CDI Stage Alternance Saisonnier Été")
+
+    id_people = models.ForeignKey(Peoples, on_delete=models.CASCADE)
+    id_advertisement = models.ForeignKey(JobAdvertisements, on_delete=models.CASCADE)
+    date_of_application = models.DateField()
+
+
