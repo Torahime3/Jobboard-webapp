b59ae385e9a8f4563a33d4bf4aa141f6dceb8e03
Ariirau <ariirau.fucks@epitech.eu>
2023-10-10 14:07:46 +0200

    Adding new route in localhost/Companies returning JSON

--- Backend/JobBoard/Companies/views.py
+++ Backend/JobBoard/Companies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def index (request):
+    data = {
+        'name': 'Name',
+        'location': 'Location',
+        'is_active': True,
+        'count': 50
+    }
+    return JsonResponse(data)
