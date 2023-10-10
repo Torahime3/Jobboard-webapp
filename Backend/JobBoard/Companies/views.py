0bed9ecff098ccee70fcab58778fbff8664e7268
Ariirau <ariirau.fucks@epitech.eu>
2023-10-10 14:19:09 +0200

    Fixing views.py

---
+++ Backend/JobBoard/Companies/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+def index(request):
+    data = {
+        'name': 'Vitor',
+        'location': 'Finland',
+        'is_active': True,
+        'count': 28
+    }
+    return JsonResponse(data)
