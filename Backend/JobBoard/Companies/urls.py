695a3238ab275b29d4ef416f60c11c5ab01fb610
Ariirau <ariirau.fucks@epitech.eu>
2023-10-10 18:58:10 +0200

    Update Companies files

--- Backend/JobBoard/Companies/urls.py
+++ Backend/JobBoard/Companies/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test', views.get_company_by_id, name='get_company_by_id'),
 ]
--- Backend/JobBoard/Companies/views.py
+++ Backend/JobBoard/Companies/views.py
@@ -1,14 +1,3 @@
 from django.shortcuts import render
 
-def index(request):
-    data_list = [
-        {
-            'name': 'Vitor',
-            'location': 'Finland',
-            'is_active': True,
-            'count': 28
-        },
-    ]
-
-    return JsonResponse(data_list)
 
