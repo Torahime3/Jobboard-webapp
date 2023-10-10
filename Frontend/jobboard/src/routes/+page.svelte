84d7421c4c7056bb42f30bb4c733d55c8dc7d4af
Torahime3 <nathandulac31@gmail.com>
2023-10-10 11:09:11 +0200

    Ajout Svelte pour front + ajout companies interface admin

---
+++ Frontend/jobboard/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<div class="container_companies">
+    <h3>{company_info.name}</h3>
+    <p>{company_info.description}</p>
+    <p>{company_info.adress}</p>
+</div>
+
+{#if company_info.name === "Epitech"}
+    <p>Je suis à Epitech</p>
+{:else}
+    <p>Je ne suis pas à Epitech</p>
+{/if}
+
+<script>
+
+    let company_info = {
+        "name": "Epitech",
+        "description": "Le futur de l'informatique, le meilleur de l'innovation",
+        "adress": "40 Boulevard de la Marquette",
+        "city": "Toulouse",
+    }
+
+</script>
