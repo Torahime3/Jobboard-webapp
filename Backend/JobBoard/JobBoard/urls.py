995e60497d0dfa961dcc7c8f29ec134fd1077171
Ariirau <ariirau.fucks@epitech.eu>
2023-10-10 10:57:25 +0200

    Modification of Jobboard
    Adding new route for the model (Companies)

--- Backend/JobBoard/JobBoard/urls.py
+++ Backend/JobBoard/JobBoard/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('my_api.urls')),
     path('my_api/', include('my_api.urls')),
+    path('Companies/',include('Companies.urls')),
     path('admin/', admin.site.urls),
 ]
 
