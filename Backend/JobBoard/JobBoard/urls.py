bad43aae8c82b3e8b7e7d41bb4ccad5e14fb2f0d
Ariirau <ariirau.fucks@epitech.eu>
2023-10-10 11:11:24 +0200

    Correction bug 'my_api'

--- Backend/JobBoard/JobBoard/urls.py
+++ Backend/JobBoard/JobBoard/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('my_api.urls')),
-    path('my_api/', include('my_api.urls')),
     path('Companies/',include('Companies.urls')),
     path('admin/', admin.site.urls),
 ]
