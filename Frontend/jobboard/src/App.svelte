a293b5f06b6100104a9705fdb120e63eff14596f
Torahime3 <nathandulac31@gmail.com>
2023-10-10 13:44:59 +0200

    Ajout d'un test avec Svelte

---
+++ Frontend/jobboard/src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+    import JobAdvertisement from './components/JobAdvertisements.svelte';
+
+	function bonjour() {
+        alert('bonjour');
+    }
+</script>
+
+<main>
+
+    <h1>Offres d'emploi</h1>
+    <JobAdvertisement number="42"/>
+    <JobAdvertisement />
+    <JobAdvertisement />
+
+    <button on:click={bonjour}>Charger les offres</button>
+
+</main>
+
+<style>
+
+    button{
+        padding: 5px 15px;
+        margin: 15px;
+    }
+
+
+</style>
