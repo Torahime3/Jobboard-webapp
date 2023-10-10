6b2254b95baf77b36a2fed00df4604fa7de06b21
Ariirau <ariirau.fucks@epitech.eu>
2023-10-10 21:15:59 +0200

    Update models.py

--- Backend/JobBoard/Peoples/models.py
+++ Backend/JobBoard/Peoples/models.py
@@ -6,14 +6,14 @@ from Companies.models import Companies
 class Peoples(models.Model):
     RolesTypes = models.TextChoices("RolesTypes", "User Recruiter")
 
-    url_profile_picture = models.CharField(max_length=255)
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
     date_of_birth = models.DateField()
     phone_number = models.CharField(max_length=20)
     email = models.CharField(max_length=255)
     domain = models.CharField(max_length=255)
-    role = models.CharField(blank=True, choices=RolesTypes.choices, max_length=10)
+    url_profile_picture = models.CharField(max_length=255)
+    role = models.CharField(blank=True,choices=RolesTypes.choices,max_length=10)
     id_company = models.ForeignKey(Companies, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
