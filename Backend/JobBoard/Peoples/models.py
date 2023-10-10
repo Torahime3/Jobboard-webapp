dd68f75f20e53971ac451058735f551a33d6f2a1
Ariirau <ariirau.fucks@epitech.eu>
2023-10-10 13:47:36 +0200

    Modification of model People

--- Backend/JobBoard/Peoples/models.py
+++ Backend/JobBoard/Peoples/models.py
@@ -3,6 +3,8 @@ from Companies.models import Companies
 
 # Create your models here.
 class Peoples(models.Model):
+    RolesTypes = models.TextChoices("RolesTypes", "User Recruiter")
+
     url_profile_picture = models.CharField(max_length=255)
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
@@ -10,8 +12,8 @@ class Peoples(models.Model):
     phone_number = models.CharField(max_length=20)
     email = models.CharField(max_length=255)
     domain = models.CharField(max_length=255)
-    role = models.CharField(max_length=50)
-    id_company = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    role = models.CharField(blank=True,choices=RolesTypes.choices,max_length=10)
+    id_company = models.ForeignKey(Companies, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return str(self.id)
