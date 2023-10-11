3cde5f94e653706dc99426516ab88f69f9eb59e2
Ariirau <ariirau.fucks@epitech.eu>
2023-10-11 09:52:28 +0200

    Fixed error Companies view

--- Backend/JobBoard/Companies/views.py
+++ Backend/JobBoard/Companies/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
 
 @api_view(['GET'])
 def getData(request):
