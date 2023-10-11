dcbcd1e0e7a20a6a3d013237097e1856e6bcb090
Ariirau <ariirau.fucks@epitech.eu>
2023-10-11 10:12:12 +0200

    Update views.py

--- Backend/JobBoard/Companies/views.py
+++ Backend/JobBoard/Companies/views.py
@@ -1,9 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from Companies.models import Companies
-
 from Companies.serializers import DataSerializer
-
 
 @api_view(['GET'])
 def getData(request):
