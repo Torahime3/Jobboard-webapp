f5a63b72d5ff1332a4db59c9f796d897cba126c9
Ariirau <ariirau.fucks@epitech.eu>
2023-10-16 11:17:24 +0200

    Add update function

--- Backend/JobBoard/Peoples/urls.py
+++ Backend/JobBoard/Peoples/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     #path('', views.get_peoples, name='all'),
     path('create', views.create, name='create'),
-    path('update', views.create, name='update'),
+    path('update', views.update, name='update'),
     path('delete', views.create, name='delete'),
     path('<str:token>', views.get_people_by_token, name='token'),
 ]
--- Backend/JobBoard/Peoples/views.py
+++ Backend/JobBoard/Peoples/views.py
@@ -7,61 +7,111 @@ from Companies.models import Companies
 from Peoples.serializers import DataSerializer
 from Login.models import Login
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_peoples(request):
     peoples = Peoples.objects.all()
     serializer = DataSerializer(peoples, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_people_by_token(request, token):
     try:
         login_to_get = Login.objects.get(token=token)
         people = Peoples.objects.get(pk=login_to_get.id_people.id)
         serializer = DataSerializer(people, many=False)
         data = {
-            'id': serializer.data['id'],
-            'url_profile_picture': serializer.data['url_profile_picture'],
-            'firstname': serializer.data['firstname'],
-            'lastname': serializer.data['lastname'],
-            'date_of_birth': serializer.data['date_of_birth'],
-            'phone_number': serializer.data['phone_number'],
-            'email': serializer.data['email'],
-            'domain': serializer.data['domain'],
-            'role': serializer.data['role'],
-            'id_company': serializer.data['id_company'],
+            "id": serializer.data["id"],
+            "url_profile_picture": serializer.data["url_profile_picture"],
+            "firstname": serializer.data["firstname"],
+            "lastname": serializer.data["lastname"],
+            "date_of_birth": serializer.data["date_of_birth"],
+            "phone_number": serializer.data["phone_number"],
+            "email": serializer.data["email"],
+            "domain": serializer.data["domain"],
+            "role": serializer.data["role"],
+            "id_company": serializer.data["id_company"],
         }
-        if(serializer.data['id_company'] == None):
-            data['company_name'] = "null"
+        if serializer.data["id_company"] == None:
+            data["company_name"] = "null"
         else:
-            company = Companies.objects.get(pk=serializer.data['id_company'])
-            data['company_name'] = company.name
-        data['message'] = 'success'
+            company = Companies.objects.get(pk=serializer.data["id_company"])
+            data["company_name"] = company.name
+        data["message"] = "success"
         return Response(data)
     except:
-        return Response({'message': 'error'})
+        return Response({"message": "error"})
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def create(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.data
-        firstname = data['firstname']
-        lastname = data['lastname']
-        date_of_birth = data['date_of_birth']
-        phone_number = data['phone_number']
+        firstname = data["firstname"]
+        lastname = data["lastname"]
+        date_of_birth = data["date_of_birth"]
+        phone_number = data["phone_number"]
         url_profile_picture = "null"
-        email = data['email']
-        domain = data['domain']
-        password = data['password']
+        email = data["email"]
+        domain = data["domain"]
+        password = data["password"]
         role = "User"
         try:
-            people = Peoples.objects.create(firstname=firstname, lastname=lastname, date_of_birth=date_of_birth, phone_number=phone_number, url_profile_picture=url_profile_picture, email=email, domain=domain, role=role)
-            passW = make_password(password, salt="jobboard", hasher='default')
+            people = Peoples.objects.create(
+                firstname=firstname,
+                lastname=lastname,
+                date_of_birth=date_of_birth,
+                phone_number=phone_number,
+                url_profile_picture=url_profile_picture,
+                email=email,
+                domain=domain,
+                role=role,
+            )
+            passW = make_password(password, salt="jobboard", hasher="default")
             tok = get_random_string(length=50)
-            login = Login.objects.create(username=str(firstname).lower(), password = passW, email=email, token=tok, id_people_id=people.id)
-            return Response({
-                    'message': 'success',
-                    'id': people.id
-                })
+            login = Login.objects.create(
+                username=str(firstname).lower(),
+                password=passW,
+                email=email,
+                token=tok,
+                id_people_id=people.id,
+            )
+            return Response({"message": "success", "id": people.id})
         except:
-            return Response({'message': 'error'})
+            return Response({"message": "error"})
+
+
+@api_view(["POST"])
+def update(request):
+    if request.method == "POST":
+        data = request.data
+        id = data["id"]
+        firstname = data["firstname"]
+        lastname = data["lastname"]
+        date_of_birth = data["date_of_birth"]
+        phone_number = data["phone_number"]
+        url_profile_picture = data["url_profile_picture"]
+        email = data["email"]
+        domain = data["domain"]
+        role = data["role"]
+        company = data["id_company"]
+        try:
+            people = Peoples.objects.get(pk=id)
+            people.firstname = firstname
+            people.lastname = lastname
+            people.date_of_birth = date_of_birth
+            people.phone_number = phone_number
+            people.url_profile_picture = url_profile_picture
+            people.email = email
+            people.domain = domain
+            people.role = role
+            if not company == "":
+                id_company = data["id_company"]
+                cmp = Companies.objects.get(pk=id_company)
+                people.id_company = cmp
+            people.save()
+
+            return Response({"message": "success"})
+        except:
+            return Response({"message": "error"})
