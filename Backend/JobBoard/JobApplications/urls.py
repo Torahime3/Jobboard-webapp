d35d695f488f5a286f1f85113dfdbcc9f1b3292c
Ariirau <ariirau.fucks@epitech.eu>
2023-10-16 11:03:58 +0200

    Fixing bugs on JobApplications

--- Backend/JobBoard/JobApplications/urls.py
+++ Backend/JobBoard/JobApplications/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_all_applications, name='all'),
-    path('<int:id>', views.get_application_by_id, name='id'),
-    path('<str:token>', views.get_application_by_token, name='token'),
-    path('create', views.create, name='create'),
+    path("", views.get_all_applications, name="all"),
+    path("create", views.create, name="create"),
+    path("<int:id>", views.get_application_by_id, name="id"),
+    path("<str:token>", views.get_application_by_token, name="token"),
 ]
--- Backend/JobBoard/JobApplications/views.py
+++ Backend/JobBoard/JobApplications/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -7,43 +8,52 @@ from JobAdvertisements.models import JobAdvertisements
 from Peoples.models import Peoples
 from Login.models import Login
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_all_applications(request):
     applications = JobApplications.objects.all()
     serializer = DataSerializer(applications, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_application_by_id(request, id):
     jobA = JobApplications.objects.get(pk=id)
     serializer = DataSerializer(jobA, many=False)
     return Response(serializer.data)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_application_by_token(request, token):
     people = Login.objects.get(token=token)
     jobA = JobApplications.objects.filter(id_people=people.id)
     serializer = DataSerializer(jobA, many=True)
     return Response(serializer.data)
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def create(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.data
-        firstname = data['firstname']
-        lastname = data['lastname']
-        email = data['email']
-        phone_number = data['phone_number']
-        id_advertisement = data['id_advertisement']
-        id_people = data['id_people']
-        date_of_application = data['date_of_application']
+        firstname = data["firstname"]
+        lastname = data["lastname"]
+        email = data["email"]
+        phone_number = data["phone_number"]
+        id_advertisement = data["id_advertisement"]
+        id_people = data["id_people"]
+        date_of_application = datetime.now()
         try:
             adv = JobAdvertisements.objects.get(pk=id_advertisement)
             people = Peoples.objects.get(pk=id_people)
-            jobA = JobApplications.objects.create(firstname=firstname, lastname=lastname, email=email, phone_number=phone_number, id_advertisement=adv, id_people=people, date_of_application=date_of_application)
-            return Response({
-                    'message': 'success',
-                    'id': jobA.id
-                })
+            jobA = JobApplications.objects.create(
+                firstname=firstname,
+                lastname=lastname,
+                email=email,
+                phone_number=phone_number,
+                id_advertisement=adv,
+                id_people=people,
+                date_of_application=date_of_application,
+            )
+            return Response({"message": "success"})
         except:
-            return Response({'message': 'error'})
+            return Response({"message": "error"})
