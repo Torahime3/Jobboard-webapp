1366670bc0e87828bfa20d63b81c01f112048b7f
Torahime3 <nathandulac31@gmail.com>
2023-10-16 15:21:57 +0200

    adding stores checkadmin + update header

--- Frontend/jobboard/src/routes/Admin.svelte
+++ Frontend/jobboard/src/routes/Admin.svelte
@@ -1,9 +1,24 @@
 <script>
 
+    import Cookies from 'js-cookie';
+    import { checkAdmin } from '../stores/checkadmin';
+
+    let token = Cookies.get('token');
+
 </script>
 
 <main>
-    <p>Interface admin</p>
+    
+        {#await checkAdmin(token)}
+            <p>Loading...</p>
+        {:then data} 
+            {#if data.message !== 'success'}
+                <p>You are not allowed to go there</p>
+            {:else}
+                <h1>Admin panel</h1>
+            {/if}
+        {/await}
+
 </main>
 
 <style>
