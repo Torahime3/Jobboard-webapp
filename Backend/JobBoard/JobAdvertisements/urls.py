cfe04b1b697efbba2764de03fee35a3f6da87b20
Ariirau <ariirau.fucks@epitech.eu>
2023-10-17 14:56:53 +0200

    Add new functions + new routes

--- Backend/JobBoard/JobAdvertisements/urls.py
+++ Backend/JobBoard/JobAdvertisements/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
       path('', views.getAllDatas, name='index'),
-      path('<int:id>', views.get_JobAdvertisementsById, name='getId')
-
+      path('<int:id>', views.get_JobAdvertisementsById, name='getId'),
+      path('<str:token>/all', views.getAll, name='getAll'),
+      path('<str:token>/create', views.create, name='create'),
+      path('<str:token>/update', views.update, name='update'),
+      # path('<str:token>/delete', views.delete, name='delete'),
 ]
--- Backend/JobBoard/JobAdvertisements/views.py
+++ Backend/JobBoard/JobAdvertisements/views.py
@@ -4,16 +4,31 @@ from JobAdvertisements.models import JobAdvertisements
 from JobAdvertisements.serializers import DataSerializer
 from JobAdvertisements.serializers2 import DataSerializerCompact
 from Companies.models import Companies
+from Login.models import Login
+from Peoples.models import Peoples
 
 @api_view(['GET'])
 def getAllDatas(request):
-
     advertisements = JobAdvertisements.objects.filter().values('contract_type', 'title', 'id_company', 'location', 'id')
     serializer = DataSerializerCompact(advertisements, many=True)
     for i in range(len(serializer.data)):
         company = Companies.objects.get(pk=serializer.data[i]['id_company'])
         serializer.data[i]['company_name'] = company.name
     return Response(serializer.data)
+
+@api_view(['GET'])
+def getAll(request,token):
+    try:
+        user = Login.objects.get(token=token)
+        role = Peoples.objects.get(pk=user.id_people_id)
+        if(role.role == 'Admin'):
+            advertisements = JobAdvertisements.objects.all()
+            serializer = DataSerializer(advertisements, many=True)
+            return Response(serializer.data)
+        else:
+            return Response({"message": "invalidAccess"})
+    except:
+        return Response({"message": "error"})
 
 @api_view(['GET'])
 def get_JobAdvertisementsById(request, id):
@@ -35,4 +50,135 @@ def get_JobAdvertisementsById(request, id):
     data['company_name'] = company.name
     return Response(data)
 
+# Method "POST"
+# Param :
+#         request -> HttpRequest, object request form Django
+#         token -> Authentication token
+# Function :
+#         create -> create a job
+# Returns :
+#         return a response, to know if the request is 'success','error' or 'invalidAccess'
+@api_view(['POST'])
+def create(request,token):
+    if request.method == 'POST':
+        try:
+            user = Login.objects.get(token=token)
+            role = Peoples.objects.get(pk=user.id_people_id)
+            if(role.role == 'Admin'):
+                if request.method == "POST":
+                    data = request.data
+                    title = data["title"]
+                    job_domain = data["job_domain"]
+                    description = data["description"]
+                    date_of_jobadvertisements = data["date_of_jobadvertisements"]
+                    location = data["location"]
+                    contract_type = data["contract_type"]
+                    duration_week = data["duration_week"]
+                    id_cmp = data["id_company"]
+                    id_ppl = data["id_people"]
+                    try:
+                        # check if id_company exist
+                        if id_cmp != "":
+                            cmp = Companies.objects.filter(pk=id_cmp)
+                            if not cmp.exists:
+                                return Response({"message": "error"})
+                        else:
+                            return Response({"message": "error"})
+                        
+                        # check if id_people exist
+                        if id_ppl != "":
+                            ppl =  Peoples.objects.filter(pk=id_ppl)
+                            if not ppl.exists:
+                               return Response({"message": "error"})
+                        else: 
+                            return Response({"message": "error"})
+                        
+                        if (contract_type.upper() not in ["CDI", "CDD", "STAGE", "ALTERNANCE", "SAISONNIER", "ÉTÉ"]):
+                            return Response({"message": "error"})
+                        
+                        # create job
+                        jobA = JobAdvertisements.objects.create(
+                        title=title,
+                        job_domain=job_domain,
+                        description=description,
+                        date_of_jobadvertisements=date_of_jobadvertisements,
+                        location=location,
+                        contract_type=contract_type.upper(),
+                        duration_week=duration_week,
+                        id_company_id=id_cmp,
+                        id_people_id=id_ppl,
+                        )
+                        return Response({"message": "success"})
+                    except:
+                        return Response({"message": "error"})
+            else:
+                return Response({"message": "invalidAccess"})
+        except:
+            return Response({"message": "error"})
 
+# Method "PUT"
+# Param :
+#         request -> HttpRequest, object request form Django
+#         token -> Authentication token
+# Function :
+#         update -> update a job with an ID
+# Returns :
+#         return a response, to know if the request is 'success','error' or 'invalidAccess'
+@api_view(["PUT"])
+def update(request,token):
+    try:
+        user = Login.objects.get(token=token)
+        role = Peoples.objects.get(pk=user.id_people_id)
+        if(role.role == 'Admin'):
+            if request.method == "PUT":
+                data = request.data
+                id = data["id"]
+                title = data["title"]
+                job_domain = data["job_domain"]
+                description = data["description"]
+                date_of_jobadvertisements = data["date_of_jobadvertisements"]
+                location = data["location"]
+                contract_type = data["contract_type"]
+                duration_week = data["duration_week"]
+                id_cmp = data["id_company"]
+                id_ppl = data["id_people"]
+                try:
+                    # check if id_company exist
+                    if id_cmp != "":
+                        cmp = Companies.objects.filter(pk=id_cmp)
+                        if cmp.exists:
+                            id_company=id_company
+                        else: return Response({"message": "error"})
+                    else:
+                        return Response({"message": "error"})
+                    
+                    # check if id_people exist
+                    if id_ppl != "":
+                        ppl =  Peoples.objects.filter(pk=id_ppl)
+                        if ppl.exists:
+                            id_people=id_people
+                        else: return Response({"message": "error"})
+                    else: 
+                        return Response({"message": "error"})
+                    
+                    # update job
+                    jobA = JobAdvertisements.objects.filter(pk=id).update(
+                    title=title,
+                    job_domain=job_domain,
+                    description=description,
+                    date_of_jobadvertisements=date_of_jobadvertisements,
+                    location=location,
+                    contract_type=contract_type,
+                    duration_week=duration_week,
+                    id_company=id_company,
+                    id_people=id_people,
+                    )
+                    return Response({"message": "success"})
+                except:
+                    return Response({"message": "error"})
+            else:
+                return Response({"message": "error"})   
+        else:
+            return Response({"message": "invalidAccess"})
+    except:
+        return Response({"message": "error"})
